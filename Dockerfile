4ab90bf23cb6ae363f2be6edab25fc5a3cc17e78
Matthias Wetzel <hobbit378@gmail.com>
2023-11-24 15:43:05 +0800

    revised signing script and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,44 @@
 FROM debian:bookworm
 
-ARG CMDTOOLS_URL="https://dl.google.com/android/repository/commandlinetools-linux-10406996_latest.zip"
+ARG CMD_TOOLS_URL="https://dl.google.com/android/repository/commandlinetools-linux-10406996_latest.zip"
 
-ENV BUILDTOOlS_PKG="build-tools;33.0.2" \
-    BUILDTOOlS_VER="33.0.2" \
-    SDK_HOME="/opt/androidsdk"
+ENV BUILD_TOOlS_PKG="build-tools" \
+    BUILD_TOOlS_VER="33.0.2" \
+    DISTRO_PKGS="   zip \
+                    less \
+                    vim \
+                    wget \
+                    sudo" \
+    JDK_PKG=openjdk-17-jre \
+    SDK_HOME="/opt/sdk"
 
-RUN echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections && \
-    apt-get clean && \
-    apt-get update && \
+RUN export DEBIAN_FRONTEND=noninteractive && \
+    apt-get update && \ 
     apt-get -y upgrade && \
-    apt-get -y install openjdk-17-jre less vim wget zip zipalign && \
+    apt-get -y install  ${DISTRO_PKGS} \
+                        ${JDK_PKG} && \
     \
-    mkdir -p /project && \
-    mkdir -p /opt/androidsdk && \
+    # download & install Android commandline tools
+    tmpfile=/tmp/cmdtools.zip && \
+    mkdir -p ${SDK_HOME} && \
+    wget -t3 -qO ${tmpfile} "${CMD_TOOLS_URL}" && \
+    unzip ${tmpfile} -d ${SDK_HOME} && \
     \
-    wget -t3 -qO /tmp/tmp.zip "${CMDTOOLS_URL}" && \
-    unzip /tmp/tmp.zip -d /opt/androidsdk && rm /tmp/tmp.zip && \
+    # download, install & link Android buildtools via sdkmanager
     ln -s "${SDK_HOME}/cmdline-tools/bin/sdkmanager" /usr/bin && \
+    yes|sdkmanager --sdk_root=${SDK_HOME} "${BUILD_TOOlS_PKG};${BUILD_TOOlS_VER}" && \
+    ln -s "${SDK_HOME}/build-tools/${BUILD_TOOlS_VER}/zipalign" /usr/bin && \
+    ln -s "${SDK_HOME}/build-tools/${BUILD_TOOlS_VER}/apksigner" /usr/bin && \
     \
-    yes|sdkmanager --sdk_root=${SDK_HOME} "${BUILDTOOlS_PKG}" && \
-    ln -s "${SDK_HOME}/build-tools/${BUILDTOOlS_VER}/apksigner" /usr/bin
+    # cleanup filesystem
+    rm -f ${tmpfile} && \
+    apt-get autoclean && \
+    apt-get autoremove
+
+ADD --chmod=555 rootfs/sign-linphone.sh /usr/bin/apksigner-linphone
+
+ENTRYPOINT [ "/bin/bash" ]
+
+WORKDIR /project
+
+
--- rootfs/sign-linphone.sh
+++ rootfs/sign-linphone.sh
@@ -5,32 +5,60 @@ BASE_DIR=${BASE_DIR:=${PWD}}
 KSSTORE=${KSSTORE:="${BASE_DIR}/keys/_default"}
 KSPWD=${KSPWD:="stdin"}
 
+function print_help(){
+
+echo "
+$0	[-d BASE_DIR ] [-i IN_DIR ] [-k KSSTORE ] [-p PWD ] [-D] <INFILE>
+
+$0 -c
+$0 -h
+
+	-d BASE_DIR		set explicit BASE_DIR path (default: '${BASE_DIR}')
+	-i IN_DIR		set explicit IN_DIR path containing APKs to sign (default: '${IN_DIR}')
+	-k KSSTORE		set explicit KSSTORE path (default: '${KSSTORE}')
+	-p PASSWORD		set password for KSSTORE (default: '${KSPWD}')
+	-c			create  keystore, set path and continue
+
+	-h			print help
+
+	-D			turn on additional debugging output
+
+	"
+}
+
+
 function sign() {
 
+	if [ ! -r "$1" ] ; then
+		echo "Error: Input File '$1' not found/not readable"
+		print_help
+		echo "Exit"
+		exit 1
+	else
+		APK=${1}
+	fi
 
 	if [ ! -r "${KSSTORE}" ] ; then
-		echo "No valid keystore found:" 
-		echo "   KSSTORE='${KSSTORE}'" 
-		echo "EXIT."
+		echo "Error: Keystore File '${KSSTORE}' not found/not readable" 
+		echo "Exit"
+		exit 1
 	fi
 
 	echo " => Processing '${APK}'"
 
 	echo "  - Clonig input file ..."
-	shopt -s nocasematch
-	CLONE_FILE=${OUT_DIR}/$(basename ${APK})
-	CLONE_FILE="${CLONE_FILE%.apk}.tmp"
+	CLONE_FILE=${APK}.clone
 	cp -v "${APK}" "${CLONE_FILE}"
-	shopt -u nocasematch
 
 	echo "  - Deleting 'META-INF/*' directory ..."
 	zip -d "${CLONE_FILE}" 'META-INF/MANIFEST.MF' 'META-INF/*.SF' 'META-INF/*.RSA'
 
+	# TODO Remove/Change in order to adjust to sign other files 
 	echo "  - Updating certificate resources ..."
 	zip  -f "${CLONE_FILE}" "assets/org.linphone.core/share/linphone/rootca.pem"
 
 	echo "  - Aligning archive ..."
-	OUT_FILE=${CLONE_FILE%.tmp}.mod.apk
+	OUT_FILE=${${CLONE_FILE}%.clone}.align
 	zipalign -v 4 "${CLONE_FILE}" "${OUT_FILE}"
 
 	echo "  - Verify alignment ..."
@@ -40,10 +68,10 @@ function sign() {
 	apksigner sign --ks "${KSSTORE}" --ks-pass "${KSPWD}" "${OUT_FILE}"
 
 	echo "  - Verify signature ..."
-	apksigner verify "${CAPK}"
+	apksigner verify "${OUT_FILE}"
 
-	echo "  - Tidy up ..."
-	rm "${CLONE_FILE}"
+	SIGNED_FILE=${${OUT_FILE}%.align}.signed
+	cp -v "${OUT_FILE}" "${SIGNED_FILE}" 
 
 	echo "    DONE"
 	
@@ -86,6 +114,9 @@ function startup() {
 			read -p "Please enter path for to be created keystore > " -i "${KSSTORE}" kspath
 			keytool -genkey -v -keystore ${kspath} -alias ${ksalias} -keyalg RSA -keysize 2048 -validity 10000
 			test -r ${kspath} && KSSTORE="${kspath}" || exit ;;
+
+		'h')
+			print_help ;;
 		
 		':')
 			echo "Option '-${OPTARG}' is missing a required argument!"
@@ -108,5 +139,5 @@ if [ "$BATCHMODE" == "TRUE" ] ; then
 	OUT_DIR=${OUT_DIR:="${BASE_DIR}/out"}
 	sign_batch
 else
-	sign
+	sign "$1"
 fi
