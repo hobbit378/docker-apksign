058d0b0b09ac8ad09c396b7f2aee7915325aa818
Matthias Wetzel <hobbit378@googlemail.com>
2024-03-21 17:00:22 +0800

    improved helper script streamlined Dockerfile, stripping of dependencies updated Github workflow

--- Dockerfile
+++ Dockerfile
@@ -1,44 +1,46 @@
-FROM debian:bookworm
+ARG DISTRO=bookworm
+FROM debian:${DISTRO}
 
-ARG CMD_TOOLS_URL="https://dl.google.com/android/repository/commandlinetools-linux-10406996_latest.zip"
+LABEL org.opencontainers.image.description \
+    "Container provides tools and helper scripts necessary to (batch-)sign Android(R) APK files \
+    including tools to create and modify a keystore required for this task \
+    The provided helper script allows for the inclusion of custom patch routines"
 
-ENV BUILD_TOOlS_PKG="build-tools" \
-    BUILD_TOOlS_VER="33.0.2" \
-    DISTRO_PKGS="   zip \
-                    less \
-                    vim \
-                    wget \
-                    sudo" \
-    JDK_PKG=openjdk-17-jre \
-    SDK_HOME="/opt/sdk"
+ARG DEFAULT_USER=signer
+ARG PROJECT_DIR=/project
+
+ENV APK_PATCH_DIR="/usr/local/share/apksign-helper/patches" \
+    KSTORE=${PROJECT_DIR}/kstore
+
 
 RUN export DEBIAN_FRONTEND=noninteractive && \
     apt-get update && \ 
-    apt-get -y upgrade && \
-    apt-get -y install  ${DISTRO_PKGS} \
-                        ${JDK_PKG} && \
-    \
-    # download & install Android commandline tools
-    tmpfile=/tmp/cmdtools.zip && \
-    mkdir -p ${SDK_HOME} && \
-    wget -t3 -qO ${tmpfile} "${CMD_TOOLS_URL}" && \
-    unzip ${tmpfile} -d ${SDK_HOME} && \
-    \
-    # download, install & link Android buildtools via sdkmanager
-    ln -s "${SDK_HOME}/cmdline-tools/bin/sdkmanager" /usr/bin && \
-    yes|sdkmanager --sdk_root=${SDK_HOME} "${BUILD_TOOlS_PKG};${BUILD_TOOlS_VER}" && \
-    ln -s "${SDK_HOME}/build-tools/${BUILD_TOOlS_VER}/zipalign" /usr/bin && \
-    ln -s "${SDK_HOME}/build-tools/${BUILD_TOOlS_VER}/apksigner" /usr/bin && \
+    apt-get upgrade -y && \
+    apt-get install -y apksigner zip zipalign \
+                        wget vim sudo && \
     \
     # cleanup filesystem
     rm -f ${tmpfile} && \
     apt-get autoclean && \
-    apt-get autoremove
+    apt-get autoremove ; \
+    \
+    # add user
+    useradd -G sudo ${DEFAULT_USER} && \
+    echo "${DEFAULT_USER}:${DEFAULT_USER}" | chpasswd && \
+    \
+    # add folder
+    mkdir ${PROJECT_DIR} && \
+    chown -vR ${DEFAULT_USER} ${PROJECT_DIR} && \
+    : "ln -s /project ~${DEFAULT_USER}/project"
 
-ADD --chmod=555 rootfs/sign-linphone.sh /usr/bin/apksigner-linphone
+VOLUME [ "${PROJECT_DIR}" ]
+
+WORKDIR "${PROJECT_DIR}"
+
+USER "${DEFAULT_USER}"
+
+ADD rootfs/ /
 
 ENTRYPOINT [ "/bin/bash" ]
-
-WORKDIR /project
 
 
---
+++ rootfs/usr/local/bin/apksign-helper.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+
+# set defaults if no explicit parameters given
+KSTORE=${KSTORE:="${PWD}/kstore"}
+KSPWD=${KSPWD:="stdin"}
+PATCH=${PATCH:=""}
+
+function usage() 
+{
+
+cat <<EOF
+$0	-c | -h
+	[-k KSTORE ] [-p PWD ] [-v] <INFILE/INDIR>
+
+	-c				create  keystore, set path and continue
+	-k KSTORE		set explicit KSTORE path (default: '${KSTORE}')
+	-p PASSWORD		set password for KSTORE (default: '${KSPWD}')
+	-m PATCH		'modify': apply selected patch to apk
+	-v				turn on bash script debugging output
+
+	-h				print usage information
+
+EOF
+
+if [ -n "$1" ] ; then
+	echo "$1"
+	exit 1
+else
+	exit
+fi
+
+}
+
+function create_keystore()
+{
+	read -p "Please enter alias for to be created keystore > " -i "my-key-alias" ksalias
+	read -p "Please enter path for to be created keystore > " -i "${KSTORE}" kspath
+	keytool -genkey -v -keystore ${kspath} -alias ${ksalias} -keyalg RSA -keysize 2048 -validity 10000
+	test -r ${kspath} && KSTORE="${kspath}" || exit
+}
+
+function apply_patch_Linphone()
+{
+	# TODO Remove/Change in order to adjust to sign other files 
+	echo "  - Updating Linphone Root Certificate resources ..."
+	zip  -f "$1" "assets/org.linphone.core/share/linphone/rootca.pem"
+}
+
+function sign() {
+
+	if [ ! -r "$1" ] ; then
+		echo "Error: Input File '$1' not found/not readable."
+		exit 1
+	else
+		APK=${1}
+	fi
+
+	if [ ! -r "${KSTORE}" ] ; then
+		echo "Error: Keystore File '${KSTORE}' not found/not readable. Maybe you need to create it first" 
+		exit 1
+	fi
+
+	echo " => Processing '${APK}'"
+
+	echo "  - Cloning & moving input file ..."
+	CLONE_FILE=${APK%.apk}.tmp
+	cp -v "${APK}" "${CLONE_FILE}"
+	ORIGN_FILE=${APK%.apk}.orig
+	mv -v "${APK}" "${ORIGN_FILE}"
+
+	echo "  - Deleting old 'META-INF/*' directory ..."
+	zip -d "${CLONE_FILE}" 'META-INF/MANIFEST.MF' 'META-INF/*.SF' 'META-INF/*.RSA'
+
+	if [ -n "${PATCH}" ] ; then
+		apply_patch_${PATCH} ${CLONE_FILE}
+	fi
+
+	echo "  - Adjusting APK archive alignment ..."
+	OUT_FILE=${CLONE_FILE}-aligned
+	if zipalign -v 4 "${CLONE_FILE}" "${OUT_FILE}" ; then
+		echo "  - Verifying alignment ..."
+		zipalign -c 4 "${OUT_FILE}"
+		mv -vf "${OUT_FILE}" "${CLONE_FILE}"
+	else
+		echo "  - Error while trying to allign ..."
+		exit 1
+	fi
+
+	echo "  - Signing package ..."
+	apksigner sign --ks "${KSTORE}" --ks-pass "${KSPWD}" "${CLONE_FILE}"
+
+	echo "  - Verifying signature ..."
+	apksigner verify "${CLONE_FILE}"
+
+	echo "  - Moving package ..."
+	mv -v "${CLONE_FILE}" "${APK}" 
+
+	echo "    DONE"
+	
+}
+
+function sign_batch() {
+	echo "Processing apk-files with filter: ${1}/*.apk"
+	for APK in ${1}/*.apk
+		do sign ${APK}
+	done
+}
+
+function startup() {
+	while getopts ":ck:p:m:hv" OPT; do
+		case $OPT in
+		
+		'k')
+			# set explicit KSTORE file path to keystore
+			test -r ${OPTARG} && KSTORE="${OPTARG}" || usage "ERROR: Keystore '${OPTARG}' does not exist/not readable!"
+			;;
+		
+		'p')
+			# set password
+			KSPWD="${OPTARG}"
+			;;
+		
+		'm')
+			# set patch to apply
+			PATCH="${OPTARG}"
+			;;
+
+		'c')
+			# create  keystore
+			create_keystore
+			exit
+			;;
+
+		'h')
+			usage
+			;;
+		
+		'v')
+			# turn on debugging
+			set -x
+			;;
+		
+		':')
+			usage "ERROR: Option '-${OPTARG}' is missing a required argument!"
+			;; 
+
+		*)
+			usage "ERROR: No such option!"
+			;;
+
+		esac
+	done
+
+	shift $((OPTIND-1))
+
+	for file in $* ; do
+		if [ -f ${file} ] ; then 
+			sign ${file}
+		elif [ -d ${file} ] ; then
+			sign_batch ${file}
+		fi
+	done 
+
+}
+
+startup $*
+
+# reset debug flag if any
+set +x
